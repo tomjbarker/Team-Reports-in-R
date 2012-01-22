7da92b5fe9e9334d0e1ec39386f33a01a9f32e64
Tom Barker <tomjbarker@gmail.com>
2012-01-22 09:23:23 -0500

    added initital bug trend plot

--- bugs.R
+++ bugs.R
@@ -1,17 +1,19 @@
-library(plotrix)
-
 ytdbugs <- read.table("2011cumulativebugs.txt", header=TRUE, sep=",", row.names="Year")
+weeklybugs <- read.table("bugsbydate.txt", header=TRUE, sep=" ", row.names="Date")
 
-#convert to percentages
-pct <- round(ytdbugs/sum(ytdbugs)*100)
-#pull out the values as vector so we can draw a pie
-slices <- c(pct$Resolved, pct$Unresolved)
+#YTD Bugs
+pct <- round(ytdbugs/sum(ytdbugs)*100) #convert to percentages
+slices <- c(pct$Resolved, pct$Unresolved) #pull out the values as vector so we can draw a pie
 lbls <- paste(colnames(pct), pct) # add percents to labels 
 lbls <- paste(lbls,"%",sep="") # ad % to labels
+
 pie(slices, labels =lbls, col=rainbow(length(colnames(pct))),main="YTD Bugs")
 
-#fan.plot(slices, labels = colnames(pct), main="Fan Plot")
-text(slices, row.names(pct), cex=0.6, pos=4, col="blue")
 
 
-#colnames(pct)
+#Trend by date
+#for(row in grep("2012", rownames(weeklybugs))){
+#	2012bugs <- weeklybugs[row]
+#}
+
+plot(weeklybugs$Total, type="b", pch=15, lty=1, col="red", main="Bug Trend Report",)
