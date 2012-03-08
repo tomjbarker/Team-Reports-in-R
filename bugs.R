edfa849d1b7b1cb9571dac0a1ff121442cae4eab
Tom Barker <tomjbarker@gmail.com>
2012-03-08 13:42:25 -0500

    added bugs by module and feature vs regression bugs

--- bugs.R
+++ bugs.R
@@ -10,10 +10,46 @@ weeklybugspng <- "/Users/tbarke000/TeamHealth/charts/bugs_weekly_trend.png"
 iterationbugs <- read.table("/Users/tbarke000/TeamHealth/data/bugstotalbyiteration.txt",  header=TRUE, sep=",", row.names="Iteration")
 iterationbugspng <- "/Users/tbarke000/TeamHealth/charts/bugs_total_by_iteration.png"
 
+featvsregress <- read.table("/Users/tbarke000/TeamHealth/data/bugfeaturevsregression.txt", header=TRUE, sep=",")
+featvsregresspng <- "/Users/tbarke000/TeamHealth/charts/bugs_ featvsregress.png"
+
+bugsbymodule <- read.table("/Users/tbarke000/TeamHealth/data/bugsbymodule.txt", header=TRUE, sep=",")
+bugsbymodulepng <- "/Users/tbarke000/TeamHealth/charts/bugs_ totalbymodule.png"
+
 drawIterationBugsRemainingOpen(bugsopen, bugsopenpng, bugsopencolumns, "Beatles")
 drawWeeklyBugTrend(weeklybugs , weeklybugspng)
 drawTotalBugsByIteration(iterationbugs, iterationbugspng)
 drawYTDBugs(ytdbugs, ytdpng)
+drawFeatureVSRegression(featvsregress, featvsregresspng)
+drawBugsByModule(bugsbymodule, bugsbymodulepng)
+
+drawBugsByModule<- function(data,png){
+	data <- data[order(data$Bugs),]
+	bugcolor <- vector()
+	for(i in 1:length(data$Bugs)){
+		if(data$Bugs[i] > 5)
+			bugcolor <- c(bugcolor, "red")
+		else{
+			bugcolor <- c(bugcolor, "orange")		
+			}
+	}
+	png(png, width = 780, height = 880, units = "px")
+	opar <- par(no.readonly=TRUE)
+	par(las=1, mar=c(10,10,10,10))		
+	barplot(data$Bugs, xlab="Number of Bugs", names.arg=data$Module, horiz=TRUE, space=1, cex.axis=0.6, cex.names=0.8, main="Open Bugs by Module", col= bugcolor)
+	par(opar)
+	dev.off()
+
+}
+
+drawFeatureVSRegression<- function(data, png){
+	bugcolors <- c("#05FF22","#F00505")
+	pcts <- 100 * (data/sum(data))
+	pcts<-t(pcts)
+	png(png)
+	barplot(as.matrix(pcts), col=bugcolors, main="Percentage of Bugs Feature vs Regression", legend=rownames(pcts))
+	dev.off()
+}
 
 drawYTDBugs <- function(ytdbugs, png){	
 #YTD Bugs
